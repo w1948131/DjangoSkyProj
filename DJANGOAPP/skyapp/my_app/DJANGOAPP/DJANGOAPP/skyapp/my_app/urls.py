f30d16265f4f760b63bab37628456325eaca2536
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-13 22:21:21 +0100

    Delete DJANGOAPP directory

--- DJANGOAPP/skyapp/my_app/DJANGOAPP/DJANGOAPP/skyapp/my_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.home,name='home'),
-    path('aboutus.html', views.about, name='about'),
-    path('contactus.html', views.contact, name='contact'),
-    path('home.html', views.home, name='home')
-]
