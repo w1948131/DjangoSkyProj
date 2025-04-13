a8e47626cd1a2fa7902e37a3dc28ed97a061218b
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-13 22:32:11 +0100

    Update urls.py
    Created url patterns for the about us and contact page

--- DJANGOAPP/skyapp/my_app/urls.py
+++ DJANGOAPP/skyapp/my_app/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='home')
+    path('', views.home,name='home'),
+    path('aboutus.html', views.about, name='about'),
+    path('contactus.html', views.contact, name='contact'),
+    path('home.html', views.home, name='home')
 ]
