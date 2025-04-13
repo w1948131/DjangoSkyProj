0a5a5e92336d415ae748c7f8b2e7ab0d42e6f91f
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-13 22:31:26 +0100

    Update views.py page request
    Created views for about us page and the requests to access those pages

--- DJANGOAPP/skyapp/my_app/views.py
+++ DJANGOAPP/skyapp/my_app/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, "my_app/home.html")
+
+def about(request):
+    return render(request, "my_app/aboutus.html")
+
+def contact(request):
+    return render(request, "my_app/contactus.html")
