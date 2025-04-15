7295b732861e986cba72599ad636a8739ae24af1
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-15 20:23:53 +0100

    views.py with register and log in functionality
    views.py with register and log in functionality

--- DJANGOAPP/skyapp/my_app/views.py
+++ DJANGOAPP/skyapp/my_app/views.py
@@ -1,7 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('engineerdashboard')
+        else:
+            return render(request, 'my_app/home.html', {'error': 'invalid password or username'})
+        
+    return render(request, 'my_app/home.html')
+
+def register_view(request):
+    if request.method == 'POST':
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        
+        if username and password:
+            if User.objects.filter(username=username).exists():
+                return render(request, 'my_app/home.html', {'error': 'username already exists'})
+            
+            user = User.objects.create_user(username=username, password=password, first_name=first_name, last_name=last_name)
+            login(request, user)
+            
+            return redirect('home')
+        else:
+            return render(request, 'my_app/home.html', {'error': 'Enter credentials in all fields'})
+    return redirect('home')
+
 def home(request):
     return render(request, "my_app/home.html")
 
@@ -10,3 +45,6 @@ def about(request):
 
 def contact(request):
     return render(request, "my_app/contactus.html")
+
+def engineerdashboard(request):
+    return render(request, "my_app/engineerdashboard.html")
