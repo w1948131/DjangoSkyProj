94ee19cc3f43fb336ec5060106758ef2c0dbf1aa
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-24 17:33:01 +0100

    Update urls.py
    added the neccesary paths for the website buttons to functions, 
    users can seamlessly got to different pages

--- DJANGOAPP/skyapp/my_app/urls.py
+++ DJANGOAPP/skyapp/my_app/urls.py
@@ -9,4 +9,9 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('register/', views.register_view, name='register'),
     path('engineerdashboard/', views.engineerdashboard, name='engineerdashboard'),
+    path('engineerdashboard.html', views.engineerdashboard, name='engineerdashboard'),
+    path('profile.html', views.profile, name='profile'),
+    path('adminpage/', views.admin_create_view, name='adminpage'),
+    path('adminpage.html/contactus.html', views.contact, name='contact'),
+    
 ]
