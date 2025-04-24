c0961f0190f5716ec989c2d15b9adb6924cf9a9c
w1948131 <164176690+w1948131@users.noreply.github.com>
2025-04-24 17:34:44 +0100

    Update models.py
    Employee model created to include the role choices on the adminpage , when admins create a new user. 
    
    as well as the for the users details, email, name etc.

--- DJANGOAPP/skyapp/my_app/models.py
+++ DJANGOAPP/skyapp/my_app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Employee(models.Model):
+   ROLE_CHOICES = [
+       ('engineer', 'Engineer'),
+       ('teamleader', 'Team Leader'),
+       ('departmentleader', 'Department Leader'),
+       ('seniormanager', 'Senior Manager'),
+       ('admin', 'Admin'),
+   ]
+
+
+   email = models.EmailField(unique=True)
+   name = models.CharField(max_length=100)
+   team_number = models.CharField(max_length=50)
+   department_number = models.CharField(max_length=50)
+   role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='engineer')
+   registered = models.BooleanField(default=False)
+
+
+   def __str__(self):
+       return self.name
