fc8c254cc8cbdaffc8ec909e616a4b919927fd09
rareschiriac <165597617+rareschiriac@users.noreply.github.com>
2025-04-29 10:25:31 -0700

    Update models.py
    This is the Models

--- DJANGOAPP/skyapp/my_app/models.py
+++ DJANGOAPP/skyapp/my_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class Employee(models.Model):
+   ROLE_CHOICES = [
+       ('engineer', 'Engineer'),
+       ('teamleader', 'Team Leader'),
+       ('departmentleader', 'Department Leader'),
+       ('seniormanager', 'Senior Manager'),
+       ('admin', 'Admin'),
+   ]
+
+
+   email = models.EmailField(unique=True)
+   name = models.CharField(max_length=100, default='')
+   username = models.CharField(max_length=100, unique=True, null=True) # set to null so it can be added to db without a value 
+   password = models.CharField(max_length=100, null=True) # set to null so it can be added to db without a value
+   team_number = models.CharField(max_length=50)
+   department_number = models.CharField(max_length=50)
+   role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='engineer')
+   registered = models.BooleanField(default=False)
+
+
+   def __str__(self):
+       return self.name
